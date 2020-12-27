8e1856e052e3113bd15fda2933040727397d680f
Aylin Görgün <34216243+Aylin1360@users.noreply.github.com>
2020-12-27 23:53:21 +0300

    The Movie DB requests complited.

--- tesrifatci.py
+++ tesrifatci.py
@@ -1,16 +1,65 @@
 import discord
 from discord.ext import commands
+
+from tmdbv3api import TMDb
+from tmdbv3api import Movie
+
+import asyncio
+from aio_timers import Timer
+
 import config
 
-client = commands.Bot(command_prefix = '_')
+client = commands.Bot(command_prefix = '%')
+tmdb = TMDb()
+
+tmdb.api_key = config.API_KEY
+tmdb.language = 'en'
+tmdb.debug = True
+
+movie = Movie()
 
 @client.event
 async def on_ready():
     print('Selam! Teşrifatçı geldi, biletleri kesiyorum.')
 
+
+@client.command
+async def help(ctx):  
+    await ctx.send("Vizyonu belirlemek için %vizyon \nGösterim saati için %gösterim {time} \nFilm arası için %ara {time} \nHakan %hakan {caption}")
+
 @client.command(aliases=['vizyonda'])
-async def vizyon(ctx):
-    await ctx.send('Hello')
+async def vizyon(ctx, *, film):
+    search = movie.search(film)
+    result = ""
+    for res in search:
+        result += "Bugün Vizyonda: " + res.title + "\n" + str(res.release_date) + "\n" + res.overview + "\n" + str(res.vote_average) 
+        break
+    await ctx.send(result)
+    await ctx.send("İyi Seyirler!")
+  
+
+@client.command()
+async def gösterim(ctx, time: int):
+    await ctx.send(f"{ctx.guild.default_role} Filmimiz Cinefakirium'da başlamak üzeredir: " + str(time) + " dk!")
+    def check(message):
+        return message.channel == ctx.channel and message.author == ctx.author and message.content.lower() == "cancel"
+    try:
+        m = await client.wait_for("message", check=check, timeout=time*60)
+        await ctx.send("Countdown cancelled")
+    except asyncio.TimeoutError:
+        await ctx.send(f"{ctx.guild.default_role} Filmimiz Cinefakirium'da gösterimde!")
+
+@client.command()
+async def ara(ctx, time: int):
+    await ctx.send(f"{ctx.guild.default_role} Film arası! " + str(time) + " dk!")
+    def check(message):
+        return message.channel == ctx.channel and message.author == ctx.author and message.content.lower() == "cancel"
+    try:
+        m = await client.wait_for("message", check=check, timeout=time*60)
+        await ctx.send("Countdown cancelled")
+    except asyncio.TimeoutError:
+        await ctx.send(f"{ctx.guild.default_role} Filmimiz Cinefakirium'da devam ediyor!")
+
 
 import random
 @client.command()
@@ -25,3 +74,5 @@ async def hakan(ctx, *, questions):
     await ctx.send(random.choice(responses))
 
 client.run(config.KEY)
+
+
