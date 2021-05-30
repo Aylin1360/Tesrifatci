172a4094c7b52213de0056edd015c47d1a30ffff
Aylin Görgün <34216243+Aylin1360@users.noreply.github.com>
2021-05-30 21:58:35 +0300

    config removed

--- config.py
+++
@@ -1,5 +0,0 @@
-KEY = "NzkyNzY0MDgwNjQ4NjE3OTg0.X-idFQ.jSdXQI1c-xjiJ9pOddzoDyrSq7U"
-
-#Moviedb API Key
-#https://developers.themoviedb.org/3/search/search-movies
-API_KEY = "30ae8bd9a11fb2a023bb2b82719d68c1"
